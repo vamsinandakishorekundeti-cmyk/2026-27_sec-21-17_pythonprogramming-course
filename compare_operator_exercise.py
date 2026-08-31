7436f23f93a513a4c7f0da377e51c76513f99a87
vamsinandakishorekundeti-cmyk <vamsinandakishorekundeti@gmail.com>
2026-08-31 16:01:03 +0530

    Add files via upload
    I am vamsi nanda kishore

---
+++ compare_operator_exercise.py
@@ -0,0 +1,8 @@
+# python comapare operator exercise
+a = 10
+b = 20
+result = a > b #greater than operator
+print("result of",a,">",b,"is",result)
+
+result = a < b #less than operator
+print("result of",a,"<",b,"is",result)
