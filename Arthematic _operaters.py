0b0c4d5c31abc86fa3f88402c2d1b65d483f9ed1
vamsinandakishorekundeti-cmyk <vamsinandakishorekundeti@gmail.com>
2026-09-18 13:59:01 +0530

    arthematic operators

---
+++ Arthematic _operaters.py
@@ -0,0 +1,24 @@
+#Arthematic_operators
+a = 9
+b = 18
+
+#Addition
+print("Sum of a and b is:", a + b) 
+
+# Subtraction
+print("Difference of a and b is:", a - b) 
+
+# Multiplication
+print("Product of a and b is:", a * b) 
+
+# Division
+print("Quotient of a and b is:", a / b)  
+
+# Modulus
+print("Remainder of a and b is:", a % b) 
+
+# Floor Division
+print("Floor division of a and b is:", a // b)     
+
+# Exponentiation
+print("a raised to the power of b is:", a ** b)
